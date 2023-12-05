b05e38b93af3625fdd7181bfa4a9e7b3a9b1f75c
Denis Basov <den4er@mail.ru>
2023-12-05 18:39:39 +0300

    js основы

---
+++ 14. JS основы/script.js
@@ -0,0 +1,40 @@
+// всплывающее окно
+//alert("hello");
+
+// вывод в консоль
+//console.log("hello");
+
+/*
+let age = 22; // объявляем переменную и присваиваем ей значение 22
+let firstName = "Иван";
+
+console.log(age); // выводим значение переменной в консоль
+console.log(firstName);
+*/
+
+/*
+let cats = 56;
+let dogs = 74;
+
+let animals = cats + dogs; // 130
+console.log(animals);
+*/
+
+// приоритет операторов
+// ()   приоритет 1000
+// * /  приоритет 100
+// + -  приоритет 10
+// =    приоритет 1
+/*
+let price = 99.99; // цена
+let discount = 7.49; // скидка
+let count = 3; // количество товара
+
+let newPrice = (price - discount) * 3;
+console.log(newPrice);*/
+
+/*
+let cars = 22;
+cars = cars - 5;
+
+console.log(cars);*/
